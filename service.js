06e799effca82abbf399c63350a66944dc7cb8b2
monthrie <wmontgomery@gmail.com>
2023-07-18 19:51:15 +0100

    user info now displayed

--- service.js
+++ service.js
@@ -62,7 +62,7 @@ function handleNameNotUniqueMessage(message) {
   }
 
 
-function sendMessage(message, address) {
+function sendMessage(message, address, callback) {
     const maxCmd = `maxima action:send poll:true to:${address} application:dmaxab data:${JSON.stringify(message)}`;
     MDS.cmd(maxCmd, function(response) {
         if (response.status) {
@@ -96,3 +96,62 @@ function sendTableRequest() {
         sendMessage(data, "MAX#0x30819F300D06092A864886F70D010101050003818D00308189028181009BB7465C454425291EBC2A851A4852F8C1B02F7A173A15780B304E2DCA663CC69AF15CA39D21914F5C1C4D20BE1066A29446F1B6AC8BC7FE1AE466D7E672C9BFAB64BA35BEE30ED8217BDB95959EA1B4410C70EF348051642876A8E99138AFCF5933E6DB3DB3ADBB3D418DBFFF30675D8BBB1A534DC5EE03740801579A73A0D10203010001#MxG18HGG6FJ038614Y8CW46US6G20810K0070CD00Z83282G60G1C0ANS2ENGJEFBYJM2SCQFR3U3KBJNP1WS9B0KG1Z2QG5T68S6N2C15B2FD7WHV5VYCKBDW943QZJ9MCZ03ESQ0TDR86PEGUFRSGEJBANN91TY2RVPQTVQSUP26TNR399UE9PPJNS75HJFTM4DG2NZRUDWP06VQHHVQSGT9ZFV0SCZBZDY0A9BK96R7M4Q483GN2T04P30GM5C10608005FHRRH4@78.141.238.36:9001");
     });
 }
+
+
+// create max address from public key and MLS
+function hasStaticMLS(callback) {
+    MDS.cmd('maxima', function (res) {
+        if (res.status) {
+            callback(res.response.staticmls === true);
+        } else {
+            MDS.log(`Error fetching static MLS: ${res.error}`);
+            callback(false);
+        }
+    });
+}
+
+function getPublicKey(callback) {
+    MDS.cmd('maxima', function (res) {
+        if (res.status) {
+            callback(null, res.response.publickey);
+        } else {
+            callback(`Couldn't fetch maxima public key ${res.error}`);
+        }
+    });
+}
+
+function getMLS(callback) {
+    MDS.cmd('maxima', function (res) {
+        if (res.status) {
+            callback(null, res.response.mls);
+        } else {
+            callback(`Couldn't fetch maxima MLS ${res.error}`);
+        }
+    });
+}
+
+function createMaxAddress(pk, mls) {
+    return `MAX#${pk}#${mls}`;
+}
+
+function getMaximaName(callback) {
+    MDS.cmd('maxima', function (res) {
+        if (res.status) {
+            callback(null, res.response.name);
+        } else {
+            callback(`Couldn't fetch Maxima name ${res.error}`);
+        }
+    });
+}
+
+function StaticMLSStatus(callback) {
+    MDS.cmd('maxima', function (res) {
+        if (res.status) {
+            callback(null, res.response.staticmls);
+        } else {
+            callback(`Couldn't fetch static MLS status ${res.error}`);
+        }
+    });
+}
+
+
